de51474564951ec8be0313ec5960882a0cd504cb
ChrstnDmbchr <christiandambacher@gmail.com>
2018-06-27 10:29:57 +0100

    modified user profile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import "../styles/UserProfile.css"
+import "../styles/UserProfile.css";
 
 class UserProfile extends Component {
   state = {
     loading: true,
     username: this.props.match.params.username,
     user: {},
-  }
-
-  componentDidMount () {
-    const { username } = this.props.match.params
-    fetch(`https://fast-hamlet-42674.herokuapp.com/api/users/${username}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(user => {
-      this.setState({ user, loading: false })
-    })
-    .catch(err => console.log(err))
   };
+
+  componentDidMount() {
+    const { username } = this.props.match.params;
+    fetch(`https://fast-hamlet-42674.herokuapp.com/api/users/${username}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(user => {
+        this.setState({ user, loading: false });
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
     const { loading, username, user } = this.state;
@@ -28,17 +28,23 @@ class UserProfile extends Component {
       <div className="userprofile">
         <div className="userprofile-banner">
           <div className="userprofile-user">
-            <h1 className="userprofile-title">{loading ? '' : 'Users'}</h1>
+            <h1 className="userprofile-title">Users</h1>
           </div>
         </div>
-        <div className="userprofile-info">
-          <p className="title is-1">{username}</p>
-          <p className="subtitle is-3"><i>{user.name}</i></p>
-          <img className="userprofile-pic" src={user.avatar_url} alt="pic" />
-        </div>
+        {loading ? (
+          <div />
+        ) : (
+          <div className="userprofile-info">
+            <p className="title is-1">{username}</p>
+            <p className="subtitle is-3">
+              <i>{user.name}</i>
+            </p>
+            <img className="userprofile-pic" src={user.avatar_url} alt="pic" />
+          </div>
+        )}
       </div>
     );
-  };
-};
+  }
+}
 
 export default UserProfile;
