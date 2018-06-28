b417034268c67bfc6f95dabd826ce69bb99b15fc
ChrstnDmbchr <christiandambacher@gmail.com>
2018-06-28 15:32:02 +0100

    modified Comment component layout

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import "../styles/Comment.css"
+import "../styles/Comment.css";
 
 class Comment extends Component {
   state = {
     votes: this.props.votes,
-    id: this.props.id,
-  }
+    id: this.props.id
+  };
 
   postUpVote = () => {
-    fetch(`https://fast-hamlet-42674.herokuapp.com/api/comments/${this.state.id}?vote=up`,{
-      method: 'PUT'
-    })
-    .then(res => {
-      return res.json();
-    })
-    .then(upvote => {
-      this.setState({
-        votes: upvote.vote_count
+    fetch(
+      `https://fast-hamlet-42674.herokuapp.com/api/comments/${this.state.id}?vote=up`,{
+        method: "PUT"
       })
-    })
-    .catch(err => console.log(err))
-  }
+      .then(res => {
+        return res.json();
+      })
+      .then(upvote => {
+        this.setState({
+          votes: upvote.vote_count
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   postDownVote = () => {
-    fetch(`https://fast-hamlet-42674.herokuapp.com/api/comments/${this.state.id}?vote=down`,{
-      method: 'PUT'
-    })
-    .then(res => {
-      return res.json();
-    })
-    .then(downvote => {
-      this.setState({
-        votes: downvote.vote_count
+    fetch(
+      `https://fast-hamlet-42674.herokuapp.com/api/comments/${this.state.id}?vote=down`,{
+        method: "PUT"
       })
-    })
-    .catch(err => console.log(err))
-  }
+      .then(res => {
+        return res.json();
+      })
+      .then(downvote => {
+        this.setState({
+          votes: downvote.vote_count
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
     const { created_by, body } = this.props;
@@ -45,24 +47,34 @@ class Comment extends Component {
     return (
       <div className="comment section animated slideInUp">
         <div className="comment-votes">
-          <i className="fa fa-angle-up fa-3x comment-vote-up" onClick={this.postUpVote}></i>
-          <i className="fa fa-angle-down fa-3x comment-vote-down" onClick={this.postDownVote}></i>
+          <i
+            className="fa fa-angle-up fa-3x comment-vote-up"
+            onClick={this.postUpVote}
+          />
+          <i
+            className="fa fa-angle-down fa-3x comment-vote-down"
+            onClick={this.postDownVote}
+          />
           <p>votes: {votes}</p>
         </div>
         <div className="comment-body">
           <div className="comment-body-delete">
-            <i className="fa fa-times-circle" aria-hidden="true" onClick={() =>console.log(`deleting comment: ${id}`)}></i>
+            <i
+              className="fa fa-times-circle"
+              aria-hidden="true"
+              onClick={() => console.log(`deleting comment: ${id}`)}
+            />
           </div>
-          <h5 className="subtitle">
-            {body}
-          </h5>
+          <h5 className="subtitle">{body}</h5>
           <div className="comment-info">
-            <p>Posted By: <Link to={`/users/${created_by}`}>{created_by}</Link></p>
+            <p>
+              Posted By: <Link to={`/users/${created_by}`}>{created_by}</Link>
+            </p>
           </div>
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default Comment;
