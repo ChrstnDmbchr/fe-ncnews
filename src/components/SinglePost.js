bf8bbdb0a1be971087f249331ea7f79242c4ef9b
ChrstnDmbchr <christiandambacher@gmail.com>
2018-06-28 14:51:22 +0100

    post notification success/failure implemented

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import Comment from "./Comment.js"
+import Comment from "./Comment.js";
 
-import "../styles/SinglePost.css"
+import "../styles/SinglePost.css";
 
 class SinglePost extends Component {
   state = {
     post: {},
     comments: [],
-    articleComment: '',
+    articleComment: "",
+    postStatus: "",
   };
 
-  componentDidMount () {
+  componentDidMount() {
     const { postId } = this.props.match.params;
     fetch(`https://fast-hamlet-42674.herokuapp.com/api/articles/${postId}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(post => {
-      this.setState({ post })
-    })
-    .catch(err => console.log(err))
+      .then(res => {
+        return res.json();
+      })
+      .then(post => {
+        this.setState({ post });
+      })
+      .catch(err => console.log(err));
 
-    fetch(`https://fast-hamlet-42674.herokuapp.com/api/articles/${postId}/comments`)
-    .then(res => {
-      return res.json();
-    })
-    .then(allComments => {
-      const { comments } = allComments
-      this.setState({ comments })
-    })
-    .catch(err => console.log(err))
+    fetch(
+      `https://fast-hamlet-42674.herokuapp.com/api/articles/${postId}/comments`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(allComments => {
+        const { comments } = allComments;
+        this.setState({ comments });
+      })
+      .catch(err => console.log(err));
   }
 
   goBack = () => {
@@ -44,30 +47,46 @@ class SinglePost extends Component {
     });
   };
 
+  clearMessage = () => {
+    this.setState({
+      postStatus: ""
+    });
+  };
+
   postArticleComment = () => {
     const { postId } = this.props.match.params;
     const { articleComment } = this.state;
 
     if (!articleComment.length) return;
 
-    fetch(`https://fast-hamlet-42674.herokuapp.com/api/articles/${postId}/comments`,{
-      method: 'POST',
-      body: JSON.stringify({comment: articleComment}),
-      headers: {'Content-Type': 'application/json'}
-    })
-    .then(res => {
-      return res.json();
-    })
-    .then(comment => {
-      this.setState({
-        articleComment: '',
-        comments : this.state.comments.concat([comment.comment]),
-      });
-    });
+    fetch(
+      `https://fast-hamlet-42674.herokuapp.com/api/articles/${postId}/comments`,
+      {
+        method: "POST",
+        body: JSON.stringify({ comment: articleComment }),
+        headers: { "Content-Type": "application/json" }
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(comment => {
+        if (comment.message !== "comment created") {
+          this.setState({
+            postStatus: "error"
+          });
+        } else {
+          this.setState({
+            articleComment: "",
+            comments: this.state.comments.concat([comment.comment]),
+            postStatus: "success"
+          });
+        }
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
-    const { post, comments, articleComment } = this.state
+    const { post, comments, articleComment, postStatus } = this.state;
     return (
       <div className="singlepost">
         <div className="singlepost-banner">
@@ -75,31 +94,68 @@ class SinglePost extends Component {
         </div>
         <div className="singlepost-info">
           <div className="singlepost-info-back">
-            <a className="button" onClick={this.goBack}>Go Back</a>
+            <a className="button" onClick={this.goBack}>
+              Go Back
+            </a>
           </div>
           <p className="title is-1">{post.title}</p>
           <p className="subtitle is-3">
             <i>{post.body}</i>
           </p>
-          <p>Created By: <Link to={`/users/${post.created_by}`}>{post.created_by}</Link></p>
+          <p>
+            Created By:{" "}
+            <Link to={`/users/${post.created_by}`}>{post.created_by}</Link>
+          </p>
         </div>
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-medium" type="text" placeholder={`Post a comment to article - ${post.title}`} onChange={this.setComment} value={articleComment}></textarea>
+            <textarea
+              className="textarea is-medium"
+              type="text"
+              placeholder={`Post a comment to article - ${post.title}`}
+              onChange={this.setComment}
+              value={articleComment}
+            />
           </div>
         </div>
         <div className="singlepost-postbutton">
-          <a className="button is-light is-medium" onClick={this.postArticleComment}>Post</a>
+          <a
+            className="button is-light is-medium"
+            onClick={this.postArticleComment}
+          >
+            Post
+          </a>
         </div>
+        {!postStatus.length ? (
+          <div />
+        ) : postStatus === "success" ? (
+          <div class="singlepost-notification notification is-primary">
+            <button class="delete" onClick={this.clearMessage} />
+            <strong>Post Successful!</strong>
+          </div>
+        ) : (
+          <div class="singlepost-notification notification is-danger">
+            <button class="delete" onClick={this.clearMessage} />
+            <strong>Error - there was an issue when trying to process your post.</strong>
+          </div>
+        )}
         <div className="singlepost-comments">
           <p className="subtitle is-3">comments:</p>
           {comments.map(comment => {
-            return <Comment key={comment._id} id={comment._id} body={comment.body} votes={comment.votes} created_by={comment.created_by} />
+            return (
+              <Comment
+                key={comment._id}
+                id={comment._id}
+                body={comment.body}
+                votes={comment.votes}
+                created_by={comment.created_by}
+              />
+            );
           })}
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default SinglePost;
